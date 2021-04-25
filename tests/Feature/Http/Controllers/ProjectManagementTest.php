a251de133b1d4e1137deb9a54338d112870f198a
Afaz Khatri <khatriafaz@gmail.com>
2021-04-25 13:21:55 +0530

    Updated the name of the project management feature test

--- tests/Feature/Http/Controllers/ProjectManagementTest.php
+++ tests/Feature/Http/Controllers/ProjectManagementTest.php
@@ -5,11 +5,10 @@ namespace Tests\Feature\Http\Controllers;
 use App\Models\Project;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
-class ProjectControllerTest extends TestCase
+class ProjectManagementTest extends TestCase
 {
     use RefreshDatabase;
 
